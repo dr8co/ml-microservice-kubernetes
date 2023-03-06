7fd2a258430d272bd8c7c8cf77cf778f29ec2845
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-03-06 19:26:29 +0300

    Upload Docker image to DockerHub

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=dr8co/sklearn:0.1
+
+# Step 2:  
+# Authenticate & tag
+docker logout
+docker login
+docker tag 919f2e8a45ce "${dockerpath}"
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push "${dockerpath}"
